508836e2cacbc0438fcad5fcd91724db7ed214fc
Ranbir Singh <77999263+rsingh124@users.noreply.github.com>
2021-10-23 15:18:59 -0600

    Ranbir create support request bar.

--- src/components/HomepageSupportRequest.js
+++ src/components/HomepageSupportRequest.js
@@ -4,8 +4,8 @@ import styles from './HomepageSupportRequest.module.css';
 const HomepageSupportRequest = () => {
     return ( 
         <div className={styles.supportRequest}>
-            <h1 className="center">Home Page Support Request</h1>
-            <p className={styles.request}>This is Ranbir's component</p>
+            <h2>Didn't find what you want?</h2>
+            <button className="submit-support"><span>Submit Support Request</span></button>
         </div>
     );
 }
