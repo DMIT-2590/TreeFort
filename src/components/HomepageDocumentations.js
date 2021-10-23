9265f011ae0ce17d41f015bcecbaa055201d9f6e
Jahmali64 <57546416+Jahmali64@users.noreply.github.com>
2021-10-22 18:06:42 -0600

    demo

--- src/components/HomepageDocumentations.js
+++ src/components/HomepageDocumentations.js
@@ -5,6 +5,7 @@ const HomepageDocumentations = () => {
     return ( 
         <div className={styles.documents}>
             <h1 className="center">Home Page Documentations</h1>
+            <p className={styles.pageColor}>This is Benedict's component.</p>
         </div>
     );
 }
