13530e6928fe00f04905847f252925701d2c1255
Jahmali64 <57546416+Jahmali64@users.noreply.github.com>
2021-11-08 23:26:09 -0700

    added search functionality

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,34 +4,32 @@ import styles from './HomepageSupportRequest.module.css'
 const contactForm = () => {
   return (
     <div className={styles.container}>
-    <div className={styles.contactImage}><img src="assets/svg/contact.svg" alt="" className={styles.imgCenter} /></div>
-    <div className={styles.contactForm}>
-      <div className={styles.formHeader}><h1>Contact Form</h1></div>
-      <form className={styles.addForm}>
-                        <div className={styles.nameLabel}>
-                            <label>Lastname:</label>
-                            <input type='text' placeholder='Surname' />
-                            <label>Firstname:</label>
-                            <input type='text' placeholder='Firstname'/>
-                        </div>
-                        <div className={styles.otherLabel}>
-                            <label>Email:</label>
-                            <input type='text' placeholder='Email' />
-                        
-                       
-                            <label>Phone:</label>
-                            <input type='text' placeholder='phone' />
-                        
-                            <label>Message:</label>
-                            <textarea type='text' placeholder='message'></textarea>
-                        
-                            <label>Attachment:</label>
-                            <input type="file" id="myfile" name="myfile" />
-                            <input type="submit" value="Submit" />
-                        </div>
-                    </form>
-                    </div>
-                </div> 
+      <div className={styles.contactImage}><img src="assets/svg/contact.svg" alt="" className={styles.imgCenter} /></div>
+      <div className={styles.contactForm}>
+        <div className={styles.formHeader}>
+          <h1>Contact Form</h1>
+        </div>
+        <form className={styles.addForm}>
+          <div className={styles.nameLabel}>
+            <label>Lastname:</label>
+            <input type='text' placeholder='Surname' />
+            <label>Firstname:</label>
+            <input type='text' placeholder='Firstname'/>
+          </div>
+          <div className={styles.otherLabel}>
+            <label>Email:</label>
+            <input type='text' placeholder='Email' />
+            <label>Phone:</label>
+            <input type='text' placeholder='phone' />                        
+            <label>Message:</label>
+            <textarea type='text' placeholder='message'></textarea>
+            <label>Attachment:</label>
+            <input type="file" id="myfile" name="myfile" />
+            <input type="submit" value="Submit" className={styles.submit} />
+          </div>
+        </form>
+      </div>
+  </div> 
   )
 }
 
--- src/components/HomepageHeader.js
+++ src/components/HomepageHeader.js
@@ -2,7 +2,8 @@ import React from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './HomepageHeader.module.css';
-import MainSearchBar from './MainSearchBar';
+import { DocSearch } from '@docsearch/react';
+import '@docsearch/css';
 
 export default function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
@@ -10,7 +11,12 @@ export default function HomepageHeader() {
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
             <p className="hero__subtitle">How can we help you?</p>
-            <MainSearchBar />
+            <DocSearch
+                apiKey = "9110916aefd88fba0ab1b95c10f4d2f8"
+                indexName = "TreeFort"
+                appId = "J276OQO1RA"
+                placeholder = "Search our help desk here"
+            />
             </div>
         </header>
     );
